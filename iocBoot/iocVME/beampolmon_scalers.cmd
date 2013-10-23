a173e6d2d0d6e26e57468861d175ce92108b5c59
OS <steffen@kph.uni-mainz.de>
2013-10-23 12:36:27 +0200

    added ioc for baempolmon

---
+++ iocBoot/iocVME/beampolmon_scalers.cmd
@@ -0,0 +1,21 @@
+#!../../bin/linux-x86/VME
+
+## You may have to change VME to something else
+## everywhere it appears in this file
+
+< envPaths
+
+cd ${TOP}
+
+## Register all support components
+dbLoadDatabase "dbd/VME.dbd"
+VME_registerRecordDeviceDriver pdbbase
+
+## Load record instances
+dbLoadRecords("db/vuprom_beampolmon.db","user=steffenHost")
+
+cd ${TOP}/iocBoot/${IOC}
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=steffenHost"
---
+++ iocBoot/iocVME/vme_scalers_beampolmon.sh
@@ -0,0 +1,5 @@
+#/bin/bash -e
+
+cd /opt/epics/apps/vme/iocBoot/iocVME/
+
+../../bin/linux-x86/VME beampolmon_scalers.cmd
